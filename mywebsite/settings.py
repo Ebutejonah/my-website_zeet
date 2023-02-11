5bdf1dce9c340efac41b0c0ce91f6221131292b6
giftedmatrix <ebutejonah386@gmail.com>
2023-02-11 12:20:24 +0100

    all domains

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["kmd2meee9i.execute-api.us-east-2.amazonaws.com","jonahebute.com","www.jonahebute.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
